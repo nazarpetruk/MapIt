561ec281d02a5299e355d7bf979fc29f0d2718a8
nazar petruk <nazarpetruk15@gmail.com>
2019-09-18 16:59:21 +0200

    Initial Commit

---
+++ MapIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapIt
+//
+//  Created by Nazar Petruk on 18/09/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
