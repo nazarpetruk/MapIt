7b241365aea9b6891be3a1eb783e18951caee210
nazar petruk <nazarpetruk15@gmail.com>
2019-09-19 19:56:58 +0200

    - adding programatically UICollection view - small refac. lbl - creating custom cell class

--- MapIt/Controller/MapVC.swift
+++ MapIt/Controller/MapVC.swift
@@ -15,15 +15,18 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var mapView: MKMapView!
    
     @IBOutlet weak var mapViewBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var pullUpView: UIView!
     
     //MARK: Variables & Constants
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius : Double = 1000
     var screenSize = UIScreen.main.bounds
+    
     var locationManager = CLLocationManager()
     var spinner: UIActivityIndicatorView?
     var progressLbl: UILabel?
+    var collectionView: UICollectionView?
+    var flowLayout = UICollectionViewFlowLayout()
     
     
     override func viewDidLoad() {
@@ -33,7 +36,15 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         mapView.showsUserLocation = true
         configureLocationServices()
         addDoubleTap()
-        mapViewBottomConstraint.constant = 0 
+        mapViewBottomConstraint.constant = 0
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
+        pullUpView.addSubview(collectionView!)
+        collectionView?.backgroundColor = #colorLiteral(red: 0.7487667203, green: 0.1050025895, blue: 0.001123019727, alpha: 1)
     }
     func addDoubleTap(){
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
@@ -44,7 +55,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     func addSwipe(){
         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown) )
         swipe.direction = .down
-        bottomView.addGestureRecognizer(swipe)
+        pullUpView.addGestureRecognizer(swipe)
     }
     @objc func animateViewDown(){
         mapViewBottomConstraint.constant = 0
@@ -64,8 +75,28 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         spinner?.style = .whiteLarge
         spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         spinner?.startAnimating()
-        bottomView.addSubview(spinner!)
+        collectionView?.addSubview(spinner!)
     }
+    func removeSpinner(){
+        if spinner != nil{
+            spinner?.removeFromSuperview()
+        }
+    }
+    func addProgressLbl(){
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: screenSize.width / 2 - 120, y: 175, width: 250, height: 40)
+        progressLbl?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLbl?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        progressLbl?.textAlignment = .center
+        
+        collectionView?.addSubview(progressLbl!)
+    }
+    func removeProgressLbl(){
+        if progressLbl != nil{
+            progressLbl?.removeFromSuperview()
+        }
+    }
+    
     
     //MARK: IBActions
     @IBAction func centerBtnPressed(_ sender: Any) {
@@ -93,9 +124,13 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     }
     @objc func dropPin(sender : UITapGestureRecognizer){
         removePin()
+        removeSpinner()
+        removeProgressLbl()
         animateViewUp()
         addSwipe()
         addSpinner()
+        addProgressLbl()
+        
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
@@ -119,6 +154,20 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         centerMapOnUserLocation()
+    }
+}
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //.count nmb from array
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        return cell!
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
 }
 
---
+++ MapIt/View/PhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  photoCell.swift
+//  MapIt
+//
+//  Created by Nazar Petruk on 19/09/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init not implemented")
+    }
+}
